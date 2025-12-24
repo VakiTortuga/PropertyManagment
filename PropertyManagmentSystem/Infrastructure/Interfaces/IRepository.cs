776c918c0b724213d15e377e4ede84d18f08eb2b
AndrewG <andrey_g.2019@mail.ru>
2025-12-24 20:24:23 +0700

    Add template repository interface

---
+++ PropertyManagmentSystem/Infrastructure/Interfaces/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyManagmentSystem.Infrastructure.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+
+        int? Add(T entity);
+        bool Update(T entity);
+        bool Delete(int id);
+
+        bool Exists(int id);
+    }
+}
